4fa411984f86cc89b6bc08749b216acb40f1bc4b
JohnReiland <reiland.shanghai@gmail.com>
2019-11-01 10:46:55 -0400

    "Complete countNRooksSolutions"

--- src/solvers.js
+++ src/solvers.js
@@ -31,9 +31,18 @@ window.findNextRooksSolution = function(n, board) {
   if (keyAndIndex === 0) {           // board is empty
     var row = 0;
     var index = 0;
-  }  else if (keyAndIndex === -1) {  // last square has a queen
-                                                      // [][][] Fill this out!!
-                                                      // [][][] Fill this out!!
+  } else if (keyAndIndex === -1) {  // last square has a queen
+    if (n === 1) {
+      return false;
+    }
+    gottenRow = board.get(n-1)
+    gottenRow[n-1] = 0;
+    board.set(n-1, gottenRow);
+    keyAndIndex = nextEmptySquare(n, board);
+    backtrack(keyAndIndex, board, row, index, n);
+    keyAndIndex = nextEmptySquare(n, board);
+    row = keyAndIndex[0];
+    index = keyAndIndex[1];
   } else {
   var row = keyAndIndex[0];
   var index = keyAndIndex[1];
@@ -57,11 +66,26 @@ window.findNextRooksSolution = function(n, board) {
         index = 0;
         continue;
       } else {
-        backtrack(keyAndIndex);
+        keyAndIndex = nextEmptySquare(n, board);
+        backtrack(keyAndIndex, board, row, index, n);
+        arr0 = board.get(0);
+        arr1 = board.get(1);           // test for signal of no solution
+        if ((arr1.reduce((a, c) => a + c) > 0) && (arr0.reduce((a, c) => a + c) === 0)) {
+          return false;
+        }
+        keyAndIndex = nextEmptySquare(n, board);
+        row = keyAndIndex[0];
+        index = keyAndIndex[1];
         continue;
       }
     }
-    placedQueens++;
+    placedQueens = 0
+    for (var j = 0; j < n; j++) {
+      arr = board.get(j);
+      if (arr.reduce((a, c) => a + c) === 1) {
+        placedQueens++;
+      }
+    }
     if (placedQueens === n) {
       break;
     } else if (index <= n - 2) {
@@ -77,13 +101,96 @@ window.findNextRooksSolution = function(n, board) {
       if (placedQueens === 0) {
         return false;
       }
-      var keyAndIndex = nextEmptySquare(n, board);
-      backtrack(keyAndIndex);
+      keyAndIndex = nextEmptySquare(n, board);
+      backtrack(keyAndIndex, board, row, index, n);
     }
   }
   return true;
 };
 
+window.findNextQueensSolution = function(n, board) {
+  var keyAndIndex = nextEmptySquare(n, board);
+
+  if (keyAndIndex === 0) {           // board is empty
+    var row = 0;
+    var index = 0;
+  }  else if (keyAndIndex === -1) {  // last square has a queen
+      debugger;                                                // [][][] Fill this out!!
+                                                      // [][][] Fill this out!!
+  } else {
+  var row = keyAndIndex[0];
+  var index = keyAndIndex[1];
+  }
+  var placedQueens = 0;
+
+  while (placedQueens < n) {
+
+    if (n > 1) {
+      arr0 = board.get(0);
+      arr1 = board.get(1);           // test for signal of no solution
+      if ((arr1.reduce((a, c) => a + c) > 0) && (arr0.reduce((a, c) => a + c) === 0)) {
+        return false;
+      }
+    }
+
+    var gottenRow = board.get(row);
+    var copiedRow = [...gottenRow];
+    gottenRow[index] = 1;                              // add a new queen
+    board.set(row, gottenRow);
+
+    if (board.hasAnyRowConflicts() ||
+        board.hasAnyColConflicts() ||
+        board.hasAnyMajorDiagonalConflicts() ||
+        board.hasAnyMinorDiagonalConflicts()) {
+      board.set(row, copiedRow);
+
+      if (index <= n - 2) {
+        index++;
+        continue;
+      } else if (row <= n - 2) {
+        row++;
+        index = 0;
+        continue;
+      } else {
+        keyAndIndex = nextEmptySquare(n, board);
+        backtrack(keyAndIndex, board, row, index, n);
+        keyAndIndex = nextEmptySquare(n, board);
+        row = keyAndIndex[0];
+        index = keyAndIndex[1];
+        continue;
+      }
+    }
+    placedQueens = 0
+    for (var j = 0; j < n; j++) {
+      arr = board.get(j);
+      if (arr.reduce((a, c) => a + c) === 1) {
+        placedQueens++;
+      }
+    }
+    if (placedQueens === n) {
+      break;
+    } else if (index <= n - 2) {
+      index++;
+      continue;
+    } else if (row <= n - 2) {
+      row++;
+      index = 0;
+      continue;
+    } else {
+      board.set(row, copiedRow);
+      placedQueens--;
+      if (placedQueens === 0) {
+        return false;
+      }
+      keyAndIndex = nextEmptySquare(n, board);
+      backtrack(keyAndIndex, board, row, index, n);
+      keyAndIndex = nextEmptySquare(n, board);
+      row = keyAndIndex[0];
+      index = keyAndIndex[1];
+    }
+  }
+  return true;
+};
 
 window.nextEmptySquare = function(n, board) {
   var rowKey = n - 1;
@@ -112,38 +219,76 @@ window.nextEmptySquare = function(n, board) {
   return 0;
 };
 
-window.backtrack = function (keyAndIndex) {
-  gottenRow = board.get(keyAndIndex[2]);        // Remove the most-recently placed queen and...
-  gottenRow[keyAndIndex][3] = 0;
-  board.set(keyAndIndex[2]);
+window.backtrack = function (keyAndIndex, board, row, index, n) {
+  var gottenRow = board.get(keyAndIndex[2]);        // Remove the most-recently placed queen and...
+  //debugger;
+  gottenRow[keyAndIndex[3]] = 0;
+  board.set(keyAndIndex[2], gottenRow);
+
+
 
   gottenRow = board.get(keyAndIndex[0]);        // ...place it into the next available square
-  gottenRow[keyAndIndex][1] = 1;
-  board.set(keyAndIndex[0]);
+  gottenRow[keyAndIndex[1]] = 1;
+  board.set(keyAndIndex[0], gottenRow);
 
-  keyAndIndex = nextEmptySquare(n, board);      // re-initialize row and index
-  if (keyAndIndex === -1) {                           // [][][] Fill this out!!
-    // [][][] Fill this out!!
+  nextKeyAndIndex = nextEmptySquare(n, board);      // re-initialize row and index
+  if (nextKeyAndIndex === -1) {
+    gottenRow[keyAndIndex[1]] = 0;
+    board.set(keyAndIndex[0], gottenRow);
+    keyAndIndex = nextEmptySquare(n, board);      // re-initialize row and index
+    row = keyAndIndex[0];
+    index = keyAndIndex[1];
+    backtrack(keyAndIndex, board, row, index, n);
+  } else {
+    keyAndIndex = nextKeyAndIndex;
+    row = keyAndIndex[0];
+    index = keyAndIndex[1];
   }
-  row = keyAndIndex[0];
-  index = keyAndIndex[1];
-  continue;
 };
 
 
 
 // return the number of nxn chessboards that exist, with n rooks placed such that none of them can attack each other
 window.countNRooksSolutions = function(n) {
-  var solutionCount = undefined; //fixme
+  var board = new Board({'n': n});  // n = 4
 
+  var solutionCount = 0;
+  debugger;
+  while (findNextRooksSolution(n, board) === true) {
+    solutionCount++;
+    console.log("solution found for board of size " + n);
+  }
+  debugger;
   console.log('Number of solutions for ' + n + ' rooks:', solutionCount);
   return solutionCount;
 };
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = undefined; //fixme
+  // if (n === 2) {
+  //   debugger;
+  // }
 
+  var solution = [];
+  var board = new Board({'n': n});  // n = 4
+
+  if (n === 0) {
+    console.log('No solution for ' + n + ' queens:', JSON.stringify(solution));
+    return solution;
+  }
+
+  if (findNextQueensSolution(n, board) === true) {
+    for (var i = 0; i < n; i++) {
+      solution.push(board.get(i));
+    }
+  } else {
+    var noSolution = new Board({'n': n});
+    for (var i = 0; i < n; i++) {
+      solution.push(noSolution.get(i));
+    }
+    console.log('No solution for ' + n + ' queens:', JSON.stringify(solution));
+    return solution;
+  }
   console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
